314c4fd404cf0bcc032a39574f5ece9272424d89
Sojib991 <gulammustafa420sujeb@gmail.com>
2025-06-09 16:50:39 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>📚 Welcome to Exam Zone!</h1>
+      <p>This is your exam preparation platform.</p>
+    </div>
+  );
+}
